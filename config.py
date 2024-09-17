7d6cc29abe27257c2c58cc611778f408ebae8493
Manh Nguyen Dong <dongmanhhust@gmail.com>
2024-09-17 10:02:50 +0700

    add MONGO_PORT

--- config.py
+++ config.py
@@ -8,4 +8,4 @@ class Config:
     HOST = os.environ.get('HOST') or "localhost"
     CELERY_PORT = int(os.environ.get('CELERY_PORT')) or 6379
     FASTAPI_PORT = int(os.environ.get('FASTAPI_PORT')) or 8000
-    
+    MONGO_PORT = int(os.environ.get('MONGO_PORT')) or 27017
